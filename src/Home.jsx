25db8a07635d695744d23668f34923cfbfb5a074
Franco <ufrancis186@gmail.com>
2025-10-21 21:10:50 +0100

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="flex mx-auto mt-24 w-[80vw] h-[70vh] bg-blue-950/80">
+
+        </div>
+    );
+}
+export default Home;
