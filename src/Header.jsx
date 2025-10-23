752a5c8c7a533695e0677ec7b4002fbf0aeeba25
Franco <ufrancis186@gmail.com>
2025-10-23 14:44:56 +0100

    header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,10 @@ import { SendIcon } from "lucide-react";
 
 function Header(){
     return(
-        <div>
-            <div>
+        <div className="flex flex-row justify-self-center justify-center items-center w-[75vw] h-[50px] bg-blue-950/80 backdrop-blur-sm  z-40 fixed top-5 rounded-md">
+            <div className="flex flex-row justify-center w-full gap-4 items-center">
                 <SendIcon color="#FFFFFF"/>
+                <p className="text-2xl text-white font-bold">MAILER</p>
             </div>
         </div>
     );
