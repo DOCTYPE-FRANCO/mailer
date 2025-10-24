26e261b1c3abf336435498d72cb119c6af524895
Franco <ufrancis186@gmail.com>
2025-10-24 15:33:51 +0100

    ok

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Navbar from "./Navbar";
 import { SendIcon } from "lucide-react";
 
-function Header(){
-    return(
-        <div className="flex flex-row justify-self-center justify-between items-center w-[75vw] h-[50px] bg-blue-950/80 backdrop-blur-sm  z-40 fixed top-5 rounded-md">
-            <div className="flex flex-row w-[500px] gap-4 items-center pl-4 hover:cursor-pointer">
+function Header() {
+    return (
+        <div className="flex flex-row gap-28 justify-between items-center w-[75vw] h-[50px] bg-blue-950/80 backdrop-blur-sm z-40 fixed top-5 left-1/2 -translate-x-1/2 rounded-md px-6">
+            <div className="flex flex-row items-center gap-4 hover:cursor-pointer">
                 <SendIcon color="#FFFFFF"/>
                 <p className="text-2xl text-white font-bold">MAILER</p>
             </div>
 
-            <Navbar />
+            <div>
+                <Navbar />
+            </div>
         </div>
     );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { MenuIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 function Navbar(){
     return(
-        <div className="flex flex-row gap-32 items-center w-[700px]">
-            <ul className="flex flex-row gap-10 text-white">
+        <div className="flex flex-row gap-20 items-center w-[500px]">
+            <ul className="hidden md:flex flex-row gap-10 text-white">
                 <Link to="/">
                     <p className="font-bold hover:scale-105 transition-all duration-300 active:text-gray-800">Home</p>
                 </Link>
@@ -19,7 +20,11 @@ function Navbar(){
                 
             </ul>
 
-            <p className="text-blue-950 font-bold bg-white p-1.5 px-4 rounded-md hover:scale-105 hover:cursor-pointer active:bg-gray-600 transition-transform duration-300">Sign Up</p>
+            <p className="hidden md:flex text-blue-950 font-bold bg-white p-1 px-5 rounded-md hover:scale-105 hover:cursor-pointer active:bg-gray-600 transition-transform duration-300">Sign Up</p>
+
+            <div className="md:hidden">
+                <MenuIcon color="#FFFFFF"/>
+            </div>
         </div>
     );
 }
