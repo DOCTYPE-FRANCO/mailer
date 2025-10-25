b42bd01eeb398e04d97fa497d32982b17d865ebb
Franco <ufrancis186@gmail.com>
2025-10-25 12:02:44 +0100

    last fix

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import { SendIcon } from "lucide-react";
 
 function Header() {
     return (
-        <div className="flex flex-row gap-28 justify-between items-center w-[75vw] h-[50px] bg-blue-950/80 backdrop-blur-sm z-30 fixed top-5 left-1/2 -translate-x-1/2 rounded-md px-6">
+        <div className="flex flex-row gap-28 justify-between items-center w-[85vw] h-[50px] bg-blue-950/80 backdrop-blur-sm z-30 fixed top-5 left-1/2 -translate-x-1/2 rounded-md px-6">
             <div className="flex flex-row items-center gap-4 hover:cursor-pointer">
                 <SendIcon color="#FFFFFF"/>
                 <p className="text-2xl text-white font-bold">MAILER</p>
