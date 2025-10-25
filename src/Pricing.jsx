9000023af91368e3ca818683290b89742338a2b8
Franco <ufrancis186@gmail.com>
2025-10-25 11:03:30 +0100

    fix

--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -3,16 +3,16 @@ import { Leaf, XIcon, CheckIcon, RocketIcon, BadgeDollarSign } from "lucide-reac
 
 function Pricing() {
     return(
-        <div className="flex flex-col z-20 mb-10">
+        <div className="flex flex-col z-20 pb-10">
             <div className="flex flex-col justify-center items-center gap-3">
-                <p className="md:text-5xl font-extrabold text-blue-950">Pricing Plans</p>
-                <div className="flex flex-col justify-center items-center gap-0.5 text-black">
+                <p className="md:text-5xl text-3xl font-extrabold text-blue-950">Pricing Plans</p>
+                <div className="flex flex-col text-sm md:text-xl text-blue-950 justify-center items-center gap-0.5 font-thin md:font-semibold">
                     <p>Choose a plan that fits your needs.</p>
                     <p>Start free and upgrade anytime - no credit card required</p>
                 </div>
             </div>
 
-            <div className="flex md:flex-row justify-center items-center gap-10 pl-3 mt-10 ">
+            <div className="flex md:flex-row flex-col justify-center items-center gap-10 pl-3 mt-10 ">
                 <div className="flex flex-col gap-3 pt-4 items-center w-[250px] h-[320px] bg-white z-10 shadow-2xl shadow-gray-700 rounded-md ">
                     <Leaf color="#00FF00" />
                     <p className="font-bold">Free</p>
@@ -41,7 +41,7 @@ function Pricing() {
                         </div>
                     </div>
 
-                    <p className="bg-green-500 p-1.5 px-10 font-bold text-white rounded-md hover:cursor-pointer hover:scale-105 transition-transform duration-300">Start for Free</p>
+                    <p className="bg-green-500 flex justify-center items-center w-[200px] h-[35px]  font-bold text-white rounded-md hover:cursor-pointer hover:scale-105 transition-transform duration-300">Start for Free</p>
                 </div>
 
                 <div className="flex flex-col gap-3 pt-4 items-center w-[250px] h-[320px] bg-white z-10 shadow-2xl shadow-gray-700 rounded-md ">
@@ -72,7 +72,7 @@ function Pricing() {
                         </div>
                     </div>
 
-                    <p className="bg-blue-700 p-1.5 px-10 font-bold text-white rounded-md hover:cursor-pointer hover:scale-105 transition-transform duration-300">Upgrade to Basic</p>
+                    <p className="bg-blue-700 flex justify-center items-center w-[200px] h-[35px] font-bold text-white rounded-md hover:cursor-pointer hover:scale-105 transition-transform duration-300">Upgrade to Basic</p>
                 </div>
 
                 <div className="flex flex-col gap-3 pt-4 items-center w-[250px] h-[320px] bg-white z-10 shadow-2xl shadow-gray-700 rounded-md ">
@@ -105,7 +105,7 @@ function Pricing() {
                         
                     </div>
 
-                    <p className="bg-purple-700 p-1.5 px-10 font-bold text-white rounded-md hover:cursor-pointer hover:scale-105 transition-transform duration-300">Start for Free</p>
+                    <p className="bg-purple-700 flex justify-center items-center w-[200px] h-[35px] font-bold text-white rounded-md hover:cursor-pointer hover:scale-105 transition-transform duration-300">Go Pro</p>
                 </div>
             </div>
         </div>
