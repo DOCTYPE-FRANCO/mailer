0292d258cbdce0afd9c82ae794bb444f4c0edc9c
Franco <ufrancis186@gmail.com>
2025-10-26 11:59:14 +0100

    mobile responsiveness

--- src/Documentation.jsx
+++ src/Documentation.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 function Documentation(){
     return(
-        <div>
-            <p className="flex justify-center font-extrabold md:text-4xl">Documentation</p>
-            <p className="flex justify-center">Learn how to get started with Mailer and use all its features</p>
+        <div className="flex flex-col justify-center items-center">
+            <p className="flex justify-center font-extrabold text-2xl md:text-4xl mb-4">Documentation</p>
+            <p className="flex justify-center max-w-[300px] text-center">Learn how to get started with Mailer and use all its features</p>
 
-            <div className="flex flex-col mt-7 pt-10 pb-10 justify-self-center items-center z-20 w-[600px] h-[330px] bg-white shadow-2xl shadow-gray-500 rounded-md">
+            <div className="flex flex-col mt-7 pt-10 pb-10 justify-self-center  items-center z-20 w-[350px] h-[450px] md:w-[600px] md:h-[330px] bg-white shadow-2xl shadow-gray-500 rounded-md">
                 <p className="text-3xl font-bold">Sending Mails</p>
 
-                <ol className="flex flex-col gap-4 mt-10">
+                <ol className="flex flex-col pl-5 md:pl-0 gap-4 mt-10">
                     <li>1. Paste Mail Addresses in the Alloted spaces</li>
                     <li>2. Separate the Addresses with comma's or spaces</li>
                     <li>3. Add the Subject of the Mail</li>
