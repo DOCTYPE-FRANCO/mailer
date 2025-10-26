081ab2fe237afde1b08c48df1b11753d2b89c85b
Franco <ufrancis186@gmail.com>
2025-10-26 11:42:12 +0100

    push

--- src/Documentation.jsx
+++ src/Documentation.jsx
@@ -5,10 +5,10 @@ function Documentation(){
             <p className="flex justify-center font-extrabold md:text-4xl">Documentation</p>
             <p className="flex justify-center">Learn how to get started with Mailer and use all its features</p>
 
-            <div className="flex flex-col mt-7 justify-self-center items-center z-20 w-[600px] h-[600px] bg-white shadow-2xl shadow-gray-500">
-                <p className="text-3xl font-bold">How To Use</p>
+            <div className="flex flex-col mt-7 pt-10 pb-10 justify-self-center items-center z-20 w-[600px] h-[330px] bg-white shadow-2xl shadow-gray-500 rounded-md">
+                <p className="text-3xl font-bold">Sending Mails</p>
 
-                <ol className="flex flex-col ">
+                <ol className="flex flex-col gap-4 mt-10">
                     <li>1. Paste mails Addresses in the Alloted spaces</li>
                     <li>2. Separate the Addresses with comma's or spaces</li>
                     <li>3. Add the Subject of the Mail</li>
